13466e2d67504b88a7179e4a9b99269f126afff0
Nkadze <nkadze@gmail.com>
2018-07-13 08:55:01 -0700

    Update configuration.rb

--- lib/inquisitor/configuration.rb
+++ lib/inquisitor/configuration.rb
@@ -5,10 +5,12 @@ module Inquisitor
       raise ArgumentError unless args
       klass = args[:class]
       puts fields
-      fields.each do |field|
-        puts "inquisitor_options_for_#{field}"
-        klass.class_attribute :"inquisitor_options_for_#{field}", instance_predicate: false
-        klass.send("inquisitor_options_for_#{field}=".to_sym, args)
+      fields.each do |f|
+        puts f.class
+        puts f
+        puts "inquisitor_options_for_#{f}"
+        klass.class_attribute :"inquisitor_options_for_#{f}", instance_predicate: false
+        klass.send("inquisitor_options_for_#{f}=".to_sym, args)
       end
     end
   end
