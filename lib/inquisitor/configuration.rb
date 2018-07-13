878f49fa085e1b28ca910fc9abb6bda4d0df4bcf
Nkadze <nkadze@gmail.com>
2018-07-13 08:32:43 -0700

    Update configuration.rb

--- lib/inquisitor/configuration.rb
+++ lib/inquisitor/configuration.rb
@@ -1,9 +1,9 @@
 module Inquisitor
   class Configuration
-    def initialize(*fields, **args)
+    def initialize(*fields, **args, caller_class: )
       raise ArgumentError unless fields
       raise ArgumentError unless args
-      klass = self
+      klass = caller_class
       fields.each do |field|
         klass.class_attribute :"inquisitor_options_for_#{field}", instance_predicate: false
         klass.send("inquisitor_options_for_#{field}=".to_sym, args)
